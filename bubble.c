bbcc12b8862ab5ea280f8bae952b9155b3326b13
KEERTHIKA M <87071460+keerthika-m05@users.noreply.github.com>
2023-07-04 20:42:27 +0530

    Update bubble.c

--- bubble.c
+++ bubble.c
@@ -16,8 +16,8 @@ void bubbleSort(int arr[], int n)
 
 		// Last i elements are already in place
 		for (j = 0; j < n - i - 1; j++)
-			if (arr[j] > arr[j + 1])
-				swap(&arr[j], &arr[j + 1]);
+			if (arr[j] > arr[j + 1]){
+				swap(&arr[j], &arr[j + 1]);}
 }
 
 /* Function to print an array */
